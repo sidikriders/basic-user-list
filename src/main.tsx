bcaf1e25bf21f971fffef097e275316c1da81af8
sidikss <sidik@sourcesage.co>
2022-08-28 07:53:35 +0700

    create basename on browser router for production

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,9 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter
+      basename={import.meta.env.MODE === "production" ? "/basic-user-list" : ""}
+    >
       <QueryParamProvider adapter={ReactRouter6Adapter}>
         <Routes>
           <Route path="/" element={<App />} />
